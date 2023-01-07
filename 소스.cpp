7c0bd25f4c36b80cf438cd558f718db804c0df56
unknown <chujayeon638@gmail.com>
2023-01-07 15:45:34 +0900

    20230107

---
+++ 소스.cpp
@@ -0,0 +1,35 @@
+////#include <stdio.h>
+////
+////int main(void)
+////{
+////	printf("Hello Everybody");
+////	printf("%d", 1234);
+////	printf("%d %d", 10, 20);
+////	return 0;
+////}
+//
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("제 이름은 홍길동 입니다.\n");
+//	printf("제 나이는 %d이고요\n",20);
+//	printf("제가 사는곳 번지수는 %d-%d 입니다",123, 456 );
+//	return 0;
+//}
+
+#include <stdio.h>
+ 
+int main(void)
+ {
+	int num1, num2;
+	int num3=30, num4 =40;
+
+	printf("num1: %d, num2: %d \n", num1, num2);
+	num1 = 10;
+	num2 = 20;
+	
+	printf("num1: %d, num2: %d \n", num1, num2);
+	printf("num3: %d, num4: %d \n", num3, num4);
+	return 0;
+} 
